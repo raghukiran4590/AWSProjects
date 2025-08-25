e29ee5dd17b075b7b659635733c92983baa85237
Raghu Kiran <raghukiran4590@gmail.com>
2025-08-24 22:18:50 -0400

    Update S3 Demo

--- S3Demo/src/main/java/com/awscodesnippet/S3DemoApplication/config/S3Config.java
+++ S3Demo/src/main/java/com/awscodesnippet/S3DemoApplication/config/S3Config.java
@@ -3,21 +3,20 @@ package com.awscodesnippet.S3DemoApplication.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
 
-    @Value("uTvWQrCZld5gnj3yL9F6MB0MVZuDIU5aSMruuv0x")
-    private String secretKey;
-
-    @Bean
-    public S3Client s3Client() {
+    @Bean("S3Client")
+    @Profile("local")
+    public S3Client s3Client(@Value("${cloud.aws.credentials.access-key}") String accessKey,
+                             @Value("uTvWQrCZld5gnj3yL9F6MB0MVZuDIU5aSMruuv0x") String secretKey) {
 //        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, "uTvWQrCZld5gnj3yL9F6MB0MVZuDIU5aSMruuv0x");
         AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey,secretKey);
         return S3Client.builder()
@@ -26,5 +25,13 @@ public class S3Config {
                 .build();
     }
 
+    @Bean("S3Client")
+    @Profile("dev")
+    public S3Client s3ClientDev() {
+        return S3Client.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
 
 }
