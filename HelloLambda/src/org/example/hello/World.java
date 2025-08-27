a4fca97d032dee232bad1fdc24be6c3425e0db91
Raghu Kiran <raghukiran4590@gmail.com>
2025-08-27 17:54:56 -0400

    update

---
+++ HelloLambda/src/org/example/hello/World.java
@@ -0,0 +1,7 @@
+package org.example.hello;
+
+public class World {
+    public void handler() {
+        System.out.println("Hello from Java Lambda Handler");
+    }
+}
---
+++ LambdaDemo/src/main/java/org/example/Hello.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Hello {
+    public void handler() {
+        System.out.println("Hello from Java Lambda Handler");
+    }
+}
